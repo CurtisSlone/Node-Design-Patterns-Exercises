5e31fa3394a4f416dbf3d7b950183703937dfb10
Curtis Slone <curtisdslone@gmail.com>
2021-09-15 21:42:15 +0200

    Fixed 5.2

--- 05-Exercises/5.2-TaskQueues/index.js
+++ 05-Exercises/5.2-TaskQueues/index.js
@@ -12,35 +12,24 @@ export class TaskQueue {
   runTask (task) {
     return new Promise((resolve, reject) => {
       this.queue.push(() => {
-        /* 
-            I'm assuming the task() provided by the args
-            uses keyword async because the return shows
-            the task() using .then
-            This suggests that the task is thenable
-
-            // Original
-            // return task().then(resolve, reject)
-
-            The modified return will be as follows:
-
-            // return await task()
-
-            Using the await keyword pauses a promise until it is resolved.
-            The thenable nature of the task() assumes that the task()
-            can be resolved
-        */
-        // Modified
-        return await task()
-
+        return task().then(resolve, reject)
       })
-      process.nextTick(this.next.bind(this))
+      /*
+        Was using Process.nextTick for async functionality
+        The next() function needed to be async using async keyword
+      */
+      this.next()
     })
   }
 
-
-  next () {
+  async next () {
     while (this.running < this.concurrency && this.queue.length) {
+      /*
+      If function did not await next task, it would execute only parent
+      link. adding the await keyword to task added the complete async functionality
+      */
       const task = this.queue.shift()
+      await task
       task().finally(() => {
         this.running--
         this.next()
